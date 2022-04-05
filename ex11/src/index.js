2fba529a205ed4eafb75079d4b18652f727ef29c
Bruno Lovric <brunoolovric@gmail.com>
2022-04-05 21:56:09 +0200

    If else

---
+++ ex11/src/index.js
@@ -0,0 +1,18 @@
+function functionElse (num) {
+
+  var result = "";
+  
+  if (num <= 5) {
+    result = "5 or smaller";
+  } else {
+    result = "Bigger than 5";
+  }
+  return result;
+}
+
+functionElse(4);
+functionElse(5);
+functionElse(6);
+functionElse(10);
+
+module.exports = functionElse;
